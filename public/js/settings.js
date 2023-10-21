424ccd52c7e6fc43df279ee2fe260ec4a2501f7c
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-10-21 08:57:06 -0300

    login suap

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-var CLIENT_ID = 'EtPS64uudgfI9faF0K3LTiw1h9h8Smu64uIDwRV7';
+var CLIENT_ID = '2ZgcvXR8Df1bid0i1HVATE9i86UcqVtVWqck6Hlx';
 var HOME_URI = 'http://localhost:8000';
 var REDIRECT_URI = HOME_URI + '/authorization-view';
 var SUAP_URL = 'https://suap.ifrn.edu.br';
