7085d6a3719328a6646b4574a104680d32f2db32
Ciro <ciro.medeiros@ifrn.edu.br>
2023-10-27 23:45:52 -0300

    (Quebrado) Traduzir nome da tabela locals

--- database/migrations/2023_10_24_124957_create_locals_table.php
+++ database/migrations/2023_10_24_124957_create_locals_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('locals', function (Blueprint $table) {
+        Schema::create('locais', function (Blueprint $table) {
             $table->id();
             $table->string('nome',255)->unique();
             $table->timestamps();
