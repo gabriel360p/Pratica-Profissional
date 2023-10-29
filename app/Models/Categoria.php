dc436383d2a46db5d0a551dcdd0ee181b2fdac1d
Ciro <ciro.medeiros@ifrn.edu.br>
2023-10-29 16:18:41 -0300

    Implementar relação Material N x N Categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Categoria extends Model
 {
@@ -11,4 +12,11 @@ class Categoria extends Model
     protected $fillable=[
         'nome',
     ];
+
+    public function materiais(): BelongsToMany {
+        return $this->belongsToMany(
+            Material::class,
+            'categoria_material_pivot',
+        )->using(CategoriaMaterialPivot::class);
+    }
 }
--- app/Models/Material.php
+++ app/Models/Material.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use App\Models\Categoria;
+use App\Models\CategoriaMaterialPivot;
 use App\Models\Item;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Material extends Model
 {
@@ -21,11 +24,14 @@ class Material extends Model
      */
     protected $table = 'materiais';
 
-    public function itens() {
+    public function itens(): HasMany {
         return $this->hasMany(Item::class);
     }
 
-    public function categorias() {
-        return $this->belongsToMany(Categoria::class);
+    public function categorias(): BelongsToMany {
+        return $this->belongsToMany(
+            Categoria::class,
+            'categoria_material_pivot'
+        )->using(CategoriaMaterialPivot::class);
     }
 }
