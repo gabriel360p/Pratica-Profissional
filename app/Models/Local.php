ef581eaf039bf2c2d7b45d3411d9b38366174e25
Ciro <ciro.medeiros@ifrn.edu.br>
2023-11-01 18:48:47 -0300

    (Quebrado) Permitir que Local tenha vários Itens

--- app/Models/Local.php
+++ app/Models/Local.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Local extends Model
 {
@@ -21,9 +22,9 @@ class Local extends Model
     ];
 
 
-    public function item()
+    public function itens(): HasMany
     {
-        return $this->hasOne(Item::class);
+        return $this->hasMany(Item::class);
     }
 
 }
